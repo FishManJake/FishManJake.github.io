b81b24bc9ed9151be47620f135e92112d495bf1c
FishManJake <tsavel2011@gmail.com>
2025-06-16 17:38:55 +0500

    .

--- script0.js
+++ script0.js
@@ -13,19 +13,19 @@ document.getElementById('userInput').addEventListener('keypress', function(event
 function checkInput() {
 	const userInput = document.getElementById('userInput').value;
 	const output = document.getElementById('outputText');
-	if (userInput === 'help') {
+	if (userInput === 'Help') {
 		output.textContent = 'About - Get Info. About Some[One/Thing] (Example - About Terminal); InetGet - Get File From Archive (Example - InetGet File.txt)';
 	}
 	else if (userInput === 'About') {
 		output.textContent = 'About [NAMEHERE]';
 	}
 	else if (userInput === 'About Terminal') {
-		output.textContent = 'Terminal Ver. 0.0.5; WritTen With HTML/CSS/JS';
+		output.textContent = 'Terminal Ver. 0.0.5; WritTen With CSS/HTML/JS';
 	}
 	else if (userInput === 'InetGet') {
 		output.textContent = 'InetGet [FILENAMEHERE]';
 	}
 	else {
-		output.textContent = 'bash: command not found';
+		output.textContent = 'Bash: Command Not Found';
 	}	
 }
