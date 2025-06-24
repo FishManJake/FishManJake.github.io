61de984b878f09e5edc3a0088361d89f60e47083
FishManJake <tsavel2011@gmail.com>
2025-06-24 13:28:53 +0500

    .

--- script0.js
+++ script0.js
@@ -46,7 +46,7 @@ function checkInput() {
     const userInput = document.getElementById('userInput').value;
     const output = document.getElementById('outputText');
     if (userInput === 'Help') {
-        output.textContent = 'About - Get Info. About Some[One/Thing] (Example - About Terminal); InetGet - Get File From Archive (Example - InetGet File.txt)';
+        output.textContent = 'About (Example - About Terminal); InetGet - (Example - InetGet dataGLDS.wav); Show Content';
     } else if (userInput === 'About Terminal') {
         output.textContent = 'Terminal Ver. 0.0.5; WritTen With CSS/HTML/JS';
     } else if (userInput.startsWith('InetGet ')) {
